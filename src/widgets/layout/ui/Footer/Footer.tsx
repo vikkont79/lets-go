ca7ca295700d454394796b406ecb1d3fc4a27ffa
vikkont79 <vvikkont79@yandex.ru>
2026-01-09 14:45:25 +0300

    feat: добавляет глобальные стили, подключает шрифт

---
+++ src/widgets/layout/ui/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={`${styles.footer} wrapper`}>
+      Footer
+    </footer>
+  )
+}
+
+export { Footer }
---
+++ src/widgets/layout/ui/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <header className={`${styles.header} wrapper`}>
+      Header
+    </header>
+  )
+}
+
+export { Header }
